dc550d354740fe036a6dd601105eac8ddb020172
Tristan Peat <tristanpeat@lawn-128-61-20-48.lawn.gatech.edu>
2021-11-05 00:56:24 -0400

    first commit

---
+++ scripts/main.py
@@ -0,0 +1,29 @@
+from github import Github, GithubException
+import logging
+import random
+import os
+# First create a Github instance:
+
+from dotenv import load_dotenv
+from consts import CONST
+
+load_dotenv()
+
+g = Github(os.getenv("GITHUB_TOKEN"))
+user = g.get_user()
+
+if CONST.REPO_NAME not in [repo.name for repo in user.get_repos()]:
+    logging.info('Repo not found, creating...')
+    user.create_repo(CONST.REPO_NAME, private=True)
+
+repo = user.get_repo(CONST.REPO_NAME)
+
+for commits in range(random.randint(1, 20)):
+    try:
+        contents = repo.get_contents(CONST.FILE_NAME)
+        commit = repo.delete_file(contents.path, "Remove test", contents.sha, branch=CONST.BRANCH_NAME)
+        logging.info(commit)
+    except GithubException as e:
+        commit = repo.create_file(CONST.FILE_NAME, "Add test", "test", branch=CONST.BRANCH_NAME)
+        logging.info(commit)
+
